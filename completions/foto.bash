7edd7bb09579e36c7ebf5658fe71b2e17dd72a15
mekb the turtle <mekbgithub@icloud.com>
2023-05-11 23:13:44 +1000

    :art: Make bash completion more readable and rename function

--- completions/foto.bash
+++ completions/foto.bash
@@ -5,7 +5,8 @@ _foto_completion() {
 	local oldifs="$IFS"
 	local isoldifs=
 	if [ -v IFS ]; then isoldifs=true; fi
-	function _contains_array() {
+
+	function _foto_completion_contains_array() {
 		local a="$1"
 		local j
 		shift
@@ -14,14 +15,15 @@ _foto_completion() {
 		done
 		return 1
 	}
+
 	opts3=('-b' '--bg')
 	opts2=("${opts3[@]}" '-p' '--pos' '-s' '--size')
 	opts1=("${opts2[@]}" '-t' '--title' '-c' '--class' '-p' '--pos' '-s' '--size' '-b' '--bg')
 	opts0=('-h' '--help' '-V' '--version')
 
-	if _contains_array "${COMP_WORDS[COMP_CWORD-3]}" "${opts3[@]}" || \
-		_contains_array "${COMP_WORDS[COMP_CWORD-2]}" "${opts2[@]}" || \
-		_contains_array "${COMP_WORDS[COMP_CWORD-1]}" "${opts1[@]}"; then
+	if _foto_completion_contains_array "${COMP_WORDS[COMP_CWORD-3]}" "${opts3[@]}" || \
+		_foto_completion_contains_array "${COMP_WORDS[COMP_CWORD-2]}" "${opts2[@]}" || \
+		_foto_completion_contains_array "${COMP_WORDS[COMP_CWORD-1]}" "${opts1[@]}"; then
 		return
 	fi
 
@@ -29,6 +31,7 @@ _foto_completion() {
 	opts="${opts3[*]} ${opts2[*]} ${opts1[*]} ${opts0[*]} -- -"
 	IFS=$'\n'
 	COMPREPLY=($(IFS=" " compgen -W "${opts}" -f -- "${COMP_WORDS[COMP_CWORD]}"))
+
 	unset IFS
 	if [[ -n "$isoldifs" ]]; then IFS="$oldifs"; fi
 }
